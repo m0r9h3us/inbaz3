e3e6a8432f1f8f3b6ad9e240510c4d33d5c36f15
jk <joha.krauss@googlemail.com>
2024-10-17 16:10:39 +0200

    add some frontend libs

---
+++ libs/frontend/primitives/src/lib/primitives.tsx
@@ -0,0 +1,11 @@
+import styles from './primitives.module.css';
+
+export function Primitives() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Primitives!</h1>
+    </div>
+  );
+}
+
+export default Primitives;
