ade7326892bd6c89ad816e530b8a3d5bfeb40896
jk <joha.krauss@googlemail.com>
2024-10-18 22:10:32 +0200

    add more icons

--- apps/inbaz3/src/components/header-logo.tsx
+++ apps/inbaz3/src/components/header-logo.tsx
@@ -1,17 +1,12 @@
 import Image from 'next/image'
 
 import inbazIcon from '../../public/images/inbaz-icon.svg'
-import jugglingIcon from '../../public/images/juggling-icon.svg'
+import jugglingIcon from '../../public/images/juggling-icon-3.svg'
 
 export const HeaderLogo = () => {
     return (
-        <div className="flex items-center">
-            <Image
-                className=""
-                priority
-                src={jugglingIcon}
-                alt="Immernoch besser als Zaubern"
-            />
+        <div className="min-h-12 flex-shrink-0">
+            <Image className="md:hidden" priority src={jugglingIcon} alt="JonglierIcon" />
             <Image
                 className="hidden md:block"
                 priority
--- apps/inbaz3/src/components/header.tsx
+++ apps/inbaz3/src/components/header.tsx
@@ -1,19 +1,16 @@
-import { HeaderLogo } from "./header-logo"
-import { HeaderNavigation } from "./header-navigation"
-import { ModeToggle } from "./mode-toggle"
-import { Search } from "./search"
-import { UserNav } from "./user-nav"
-
+import { HeaderLogo } from './header-logo'
+import { HeaderNavigation } from './header-navigation'
+import { ModeToggle } from './mode-toggle'
+import { Search } from './search'
+import { UserNav } from './user-nav'
 
 export const Header = () => {
-
     return (
         <div className="flex h-16 items-center px-4 justify-between flex-1 border-b">
-            <div className="flex gap-4">
+            <div className="flex gap-8 md:gap-16 ">
                 <HeaderLogo />
                 <HeaderNavigation />
             </div>
-
 
             <div className="flex gap-4 items-center">
                 <Search />
@@ -21,6 +18,5 @@ export const Header = () => {
                 <UserNav />
             </div>
         </div>
-
     )
 }
