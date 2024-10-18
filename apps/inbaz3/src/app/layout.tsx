ffdc58413af7f3d5916ae7c9d287b6c0e63bde53
jk <joha.krauss@googlemail.com>
2024-10-18 20:00:18 +0200

    cleanup layout

--- apps/inbaz3/src/app/layout.tsx
+++ apps/inbaz3/src/app/layout.tsx
@@ -34,24 +34,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {/* <div className="border-b">
-            <div className="flex h-16 items-center px-4">
-              <TeamSwitcher />
-              <MainNav className="mx-6" />
-              <div className="ml-auto flex items-center space-x-4">
-                <Search />
-                <UserNav />
-              </div>
-            </div>
-          </div> */}
           <Header />
-          {/* <Header>
-            <Header.Navigation>
-              <NavigationMenuExample />
-            </Header.Navigation>
-            <ModeToggle />
-          </Header> */}
-
           {children}
         </ThemeProvider>
       </body>
