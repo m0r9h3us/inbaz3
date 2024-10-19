31c6c8b89f92329d3d82f9f66fef5b72c2556ae6
jk <joha.krauss@googlemail.com>
2024-10-19 20:04:27 +0200

    recreate icons

--- apps/inbaz3/src/components/header-logo.tsx
+++ apps/inbaz3/src/components/header-logo.tsx
@@ -1,18 +1,10 @@
-import Image from 'next/image'
-
-import inbazIcon from '../../public/images/inbaz-icon.svg'
-import jugglingIcon from '../../public/images/juggling-icon-3.svg'
+import JugglingIcon from './juggling-icon'
 
 export const HeaderLogo = () => {
     return (
-        <div className="min-h-12 flex-shrink-0">
-            <Image className="md:hidden" priority src={jugglingIcon} alt="JonglierIcon" />
-            <Image
-                className="hidden md:block"
-                priority
-                src={inbazIcon}
-                alt="Immernoch besser als Zaubern"
-            />
+        <div className="flex min-h-12 flex-shrink-0 gap-4">
+            <JugglingIcon className="w-12 h-12 fill-violet-400" />
+            <span className="hidden text-5xl text-violet-400 md:block">DEBAZ</span>
         </div>
     )
 }
