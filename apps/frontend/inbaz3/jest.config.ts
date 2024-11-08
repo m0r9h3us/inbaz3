d1396a298e6f6a1d0570eccc144a030f6e3bfd1e
jk <joha.krauss@googlemail.com>
2024-11-08 17:32:37 +0100

    move frontend app to frontend folder

--- apps/frontend/inbaz3/jest.config.ts
+++ apps/frontend/inbaz3/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
     displayName: 'inbaz3',
-    preset: '../../jest.preset.js',
+    preset: '../../../jest.preset.js',
     transform: {
         '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-        '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+        '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }]
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    coverageDirectory: '../../coverage/apps/inbaz3',
-}
+    coverageDirectory: '../../../coverage/apps/inbaz3'
+};
