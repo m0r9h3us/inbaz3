6cafcc4f7c59383341d1acd4d8210bde8d030d19
jk <joha.krauss@googlemail.com>
2024-11-22 17:01:47 +0100

    adjust header

--- apps/frontend/juggling-hub/src/components/header/header-logo.tsx
+++ apps/frontend/juggling-hub/src/components/header/header-logo.tsx
@@ -2,11 +2,13 @@ import JugglingIcon from '../../icons/juggling-icon';
 
 export const HeaderLogo = () => {
     return (
-        <div className="flex min-h-12 flex-shrink-0 gap-4">
-            <JugglingIcon className="w-12 h-12 fill-violet-400" />
-            <span className="hidden text-5xl text-violet-400 md:block">
-                DEBAZ
-            </span>
+        <div className="flex min-h-12 flex-shrink-0 gap-4 items-center">
+            <JugglingIcon className="w-10 h-10 fill-violet-600" />
+            <div className="hidden md:block">
+                <span className="text-4xl text-violet-400">
+                    JUGGLING <span className="text-4xl text-violet-600">HUB</span>
+                </span>
+            </div>
         </div>
     );
 };
